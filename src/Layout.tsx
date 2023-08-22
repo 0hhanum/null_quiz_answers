0bcb47360f202141122058e5aa6f757a130e388e
ohhanum <rntls123@naver.com>
2023-08-22 16:48:51 +0900

    Feat(Layout): Add logo

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -3,7 +3,22 @@ import styled from "styled-components";
 
 const Main = styled.main``;
 const Layout: React.FC<any> = ({ children }) => {
-    return <Main className="container">{children}</Main>;
+    return (
+        <Main className="container">
+            <section>
+                <header>
+                    <nav>
+                        <img
+                            src="/images/logo.svg"
+                            width="35%"
+                            style={{ marginTop: 10 }}
+                        />
+                    </nav>
+                </header>
+            </section>
+            <section>{children}</section>
+        </Main>
+    );
 };
 
 export default Layout;
