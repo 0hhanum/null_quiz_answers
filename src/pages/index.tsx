62a16a30a775d9c4938a9cc74bafe0a9693ca1e2
ohhanum <rntls123@naver.com>
2023-08-28 15:05:10 +0900

    Feat(index): Add detail pages links on index page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,45 @@
 import React from "react";
-import { PageProps, HeadFC } from "gatsby";
+import { PageProps, HeadFC, graphql } from "gatsby";
 import Helmet from "../components/Helmet";
 import { styled } from "styled-components";
 
 const Container = styled.div`
-    height: 60vh;
     display: flex;
+    flex-direction: column;
     justify-content: center;
-    align-items: center;
+    align-items: start;
 `;
-const IndexPage: React.FC<PageProps> = () => {
-    return <Container>Welcome to the answer hub of null.</Container>;
+const MdxList = styled.ul``;
+const IndexPage = ({
+    data: { allMdx },
+}: PageProps<Queries.getAllMdxesQuery>) => {
+    return (
+        <Container>
+            <h1>Welcome to the answer hub of null.</h1>
+            <MdxList>
+                {allMdx.edges?.map((mdx) => (
+                    <li>
+                        <a href={mdx.node.id}>{mdx.node.frontmatter?.title}</a>
+                    </li>
+                ))}
+            </MdxList>
+        </Container>
+    );
 };
 
 export default IndexPage;
 export const Head: HeadFC = () => <Helmet />;
+export const query = graphql`
+    query getAllMdxes {
+        allMdx {
+            edges {
+                node {
+                    id
+                    frontmatter {
+                        title
+                    }
+                }
+            }
+        }
+    }
+`;
