dcf30fcad5922a6368ca8736b9534cdf73367289
ohhanum <rntls123@naver.com>
2023-08-30 16:42:50 +0900

    test

--- .github/scripts/firebase.js
+++ .github/scripts/firebase.js
@@ -1,46 +1,47 @@
-const changedMdxFilePaths = process.env.CHANGED_FILES
-    ? process.env.CHANGED_FILES.split(" ")
-          .filter((fileName) => {
-              return Boolean(fileName) && fileName.includes(".mdx");
-          })
-          .map((fileName) => fileName.replace('"', ""))
-    : [];
-const mdxParser = require("./mdxParser");
-const dotenv = require("dotenv");
-dotenv.config();
+console.log(process.env.FIREBASE_DB_PATH);
+// const changedMdxFilePaths = process.env.CHANGED_FILES
+//     ? process.env.CHANGED_FILES.split(" ")
+//           .filter((fileName) => {
+//               return Boolean(fileName) && fileName.includes(".mdx");
+//           })
+//           .map((fileName) => fileName.replace('"', ""))
+//     : [];
+// const mdxParser = require("./mdxParser");
+// const dotenv = require("dotenv");
+// dotenv.config();
 
-const admin = require("firebase-admin");
-// console.log(process.env.FIREBASE_CERTIFICATION_PATH);
-const path = require("path");
+// const admin = require("firebase-admin");
+// // console.log(process.env.FIREBASE_CERTIFICATION_PATH);
+// const path = require("path");
 
-const firebaseCertification = require(path.join(__dirname, "/credential.json"));
-admin.initializeApp({
-    credential: admin.credential.cert(firebaseCertification),
-    databaseURL: process.env.FIREBASE_DB_PATH,
-});
+// const firebaseCertification = require(path.join(__dirname, "/credential.json"));
+// admin.initializeApp({
+//     credential: admin.credential.cert(firebaseCertification),
+//     databaseURL: process.env.FIREBASE_DB_PATH,
+// });
 
-const db = admin.database();
-const quizRef = db.ref("quiz");
+// const db = admin.database();
+// const quizRef = db.ref("quiz");
 
-const updateQuizData = async (mdxFilePath) => {
-    const { title, tags, question, questionType, choices, answer, slug } =
-        mdxParser(mdxFilePath);
-    await quizRef.child(slug).set({
-        title,
-        tags,
-        question,
-        questionType,
-        choices,
-        answer,
-    });
-};
+// const updateQuizData = async (mdxFilePath) => {
+//     const { title, tags, question, questionType, choices, answer, slug } =
+//         mdxParser(mdxFilePath);
+//     await quizRef.child(slug).set({
+//         title,
+//         tags,
+//         question,
+//         questionType,
+//         choices,
+//         answer,
+//     });
+// };
 
-(async () => {
-    try {
-        const updatePromises = changedMdxFilePaths.map(updateQuizData);
-        await Promise.all(updatePromises);
-        console.log("All updates completed.");
-    } catch (error) {
-        console.error("An error occurred:", error);
-    }
-})();
+// (async () => {
+//     try {
+//         const updatePromises = changedMdxFilePaths.map(updateQuizData);
+//         await Promise.all(updatePromises);
+//         console.log("All updates completed.");
+//     } catch (error) {
+//         console.error("An error occurred:", error);
+//     }
+// })();
