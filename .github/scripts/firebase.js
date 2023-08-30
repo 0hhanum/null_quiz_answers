0b53759e8b7615d892ce4060d554e1097bda3104
ohhanum <rntls123@naver.com>
2023-08-30 17:41:29 +0900

    test

--- .github/scripts/firebase.js
+++ .github/scripts/firebase.js
@@ -1,48 +1,54 @@
-console.log(123 + +process.env.TEST);
-console.log(process.env.TEST);
-// const changedMdxFilePaths = process.env.CHANGED_FILES
-//     ? process.env.CHANGED_FILES.split(" ")
-//           .filter((fileName) => {
-//               return Boolean(fileName) && fileName.includes(".mdx");
-//           })
-//           .map((fileName) => fileName.replace('"', ""))
-//     : [];
-// const mdxParser = require("./mdxParser");
-// const dotenv = require("dotenv");
-// dotenv.config();
+const changedMdxFilePaths = process.env.CHANGED_FILES
+    ? process.env.CHANGED_FILES.split(" ")
+          .filter((fileName) => {
+              return Boolean(fileName) && fileName.includes(".mdx");
+          })
+          .map((fileName) => fileName.replace('"', ""))
+    : [];
+const mdxParser = require("./mdxParser");
+const dotenv = require("dotenv");
+dotenv.config();
 
-// const admin = require("firebase-admin");
-// // console.log(process.env.FIREBASE_CERTIFICATION_PATH);
-// const path = require("path");
+const admin = require("firebase-admin");
+const credential = {
+    type: "service_account",
+    project_id: process.env.PROJECT_ID,
+    universe_domain: "googleapis.com",
+    private_key_id: process.env.PRIVATE_KEY,
+    client_email: process.env.CLIENT_EMAIL,
+    client_id: process.env.CLIENT_ID,
+    auth_uri: process.env.AUTH_URI,
+    token_uri: process.env.TOKEN_URI,
+    auth_provider_x509_cert_url: process.env.AUTH_PROVIDER_X509_CERT_URL,
+    client_x509_cert_url: process.env.CLIENT_X509_CERT_URL,
+};
+admin.initializeApp({
+    credential: admin.credential.cert(credential),
+    databaseURL: process.env.FIREBASE_DB_PATH,
+});
 
-// const firebaseCertification = require(path.join(__dirname, "/credential.json"));
-// admin.initializeApp({
-//     credential: admin.credential.cert(firebaseCertification),
-//     databaseURL: process.env.FIREBASE_DB_PATH,
-// });
+const db = admin.database();
+const quizRef = db.ref("quiz");
 
-// const db = admin.database();
-// const quizRef = db.ref("quiz");
+const updateQuizData = async (mdxFilePath) => {
+    const { title, tags, question, questionType, choices, answer, slug } =
+        mdxParser(mdxFilePath);
+    await quizRef.child(slug).set({
+        title,
+        tags,
+        question,
+        questionType,
+        choices,
+        answer,
+    });
+};
 
-// const updateQuizData = async (mdxFilePath) => {
-//     const { title, tags, question, questionType, choices, answer, slug } =
-//         mdxParser(mdxFilePath);
-//     await quizRef.child(slug).set({
-//         title,
-//         tags,
-//         question,
-//         questionType,
-//         choices,
-//         answer,
-//     });
-// };
-
-// (async () => {
-//     try {
-//         const updatePromises = changedMdxFilePaths.map(updateQuizData);
-//         await Promise.all(updatePromises);
-//         console.log("All updates completed.");
-//     } catch (error) {
-//         console.error("An error occurred:", error);
-//     }
-// })();
+(async () => {
+    try {
+        const updatePromises = changedMdxFilePaths.map(updateQuizData);
+        await Promise.all(updatePromises);
+        console.log("All updates completed.");
+    } catch (error) {
+        console.error("An error occurred:", error);
+    }
+})();
