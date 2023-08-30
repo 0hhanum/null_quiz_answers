9482f85eb42df37198aa60cf53983767336c85b6
ohhanum <rntls123@naver.com>
2023-08-30 16:46:20 +0900

    test

--- .github/scripts/firebase.js
+++ .github/scripts/firebase.js
@@ -9,6 +9,7 @@
 const dotenv = require("dotenv");
 dotenv.config();
 console.log(process.env.FIREBASE_DB_PATH);
+console.log(process.env.test);
 
 // const admin = require("firebase-admin");
 // // console.log(process.env.FIREBASE_CERTIFICATION_PATH);
