522435c773e0ed3ab5f2b264f747e48767310e02
hanum <rntls123@naver.com>
2023-09-13 23:12:41 +0900

    Add Quiz: 생성자 함수와 prototype 메서드

--- contents/format.ts
+++ contents/format.ts
@@ -2,13 +2,14 @@
 
 type questionType = "객관식" | "주관식" | "OX" | "빈칸";
 interface IFrontmatter {
-    title: string;
-    date: Date; // YYYY-MM-DD (gatsby automately changes date object)
-    tags: string[];
-    relatedLinks: string[];
-    question: string[];
-    questionType: questionType;
-    choices?: string[] | string[][]; // 객관식 | 빈칸
-    answer: string[];
-    slug: string;
+  title: string;
+  date: Date; // YYYY-MM-DD (gatsby automately changes date object)
+  tags: string[];
+  relatedLinks: string[];
+  question: string[];
+  questionType: questionType;
+  choices?: string[] | string[][]; // 객관식 | 빈칸
+  answer: string[];
+  slug: string;
+  level: number; // 1, 2, 3, 4, 5
 }
