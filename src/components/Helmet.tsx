9badc7d8e56e0e737d9bbaf3acad2ad52cc4fdb1
hanum <rntls123@naver.com>
2023-11-08 15:35:06 +0700

    Feat: Force dark mode in picoCSS

--- src/components/Helmet.tsx
+++ src/components/Helmet.tsx
@@ -3,30 +3,31 @@ import React from "react";
 import { makeAssetPath } from "../utils";
 
 interface IHelmet {
-    title?: string;
+  title?: string;
 }
 export default function Helmet({ title }: IHelmet) {
-    const titleQueryResponse = useStaticQuery<Queries.getTitleQuery>(graphql`
-        query getTitle {
-            site {
-                siteMetadata {
-                    title
-                }
-            }
+  const titleQueryResponse = useStaticQuery<Queries.getTitleQuery>(graphql`
+    query getTitle {
+      site {
+        siteMetadata {
+          title
         }
-    `);
-    return (
-        <>
-            <title>
-                {title
-                    ? `${title} | ${titleQueryResponse?.site?.siteMetadata?.title}`
-                    : titleQueryResponse?.site?.siteMetadata?.title}
-            </title>
-            <link
-                rel="shortcut icon"
-                type="image/x-icon"
-                href={makeAssetPath("/images/favicon.ico")}
-            ></link>
-        </>
-    );
+      }
+    }
+  `);
+  return (
+    <>
+      <html data-theme="dark" />
+      <title>
+        {title
+          ? `${title} | ${titleQueryResponse?.site?.siteMetadata?.title}`
+          : titleQueryResponse?.site?.siteMetadata?.title}
+      </title>
+      <link
+        rel="shortcut icon"
+        type="image/x-icon"
+        href={makeAssetPath("/images/favicon.ico")}
+      ></link>
+    </>
+  );
 }
